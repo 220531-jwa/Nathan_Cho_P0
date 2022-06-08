5e39462dab1035721edd88cef9ebd8729177a23e
nadancho <nathan846@revature.net>
2022-06-08 13:49:05 -0600

    adding project 0 for first time

---
+++ src/main/java/dev/cho/model/Account.java
@@ -0,0 +1,23 @@
+package dev.cho.model;
+
+public class Account {
+	private int accountNumber;
+	private double balance;
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void setAccountNumber( int accountNumber) {
+		this.accountNumber = accountNumber;
+		
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setDouble(double balance) {
+		this.balance = balance;
+	}
+}
